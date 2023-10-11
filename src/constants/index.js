bd64b88bfa98e0ac1df71e7a053a6f1e664a2a92
Molise <61286622+molisemahasele@users.noreply.github.com>
2023-10-11 05:21:34 -0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,7 +135,7 @@ import {
       iconBg: "#E6DEDD",
       date: "2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaiions using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
